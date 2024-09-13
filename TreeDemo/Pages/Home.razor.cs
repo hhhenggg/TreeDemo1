b133994cae879141e78ba85e999ad43874631f47
wooheng <13811469827@139.com>
2024-09-13 22:53:31 +0800

    添加项目文件。

---
+++ TreeDemo/Pages/Home.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TreeDemo.Pages
+{
+    public partial class Home: ComponentBase
+    {
+        private List<NodeType> soruces = new List<NodeType>();
+        protected override Task OnInitializedAsync()
+        {
+            soruces = new()
+            {
+                new NodeType()
+                {
+                    Name="root",
+                    Id=0,
+                    ParentId=1000,
+                },
+                new NodeType()
+                {
+                    Name="c1",
+                    Id=1,
+                    ParentId=0,
+                },
+                new NodeType()
+                {
+                    Name="root",
+                    Id=2,
+                    ParentId=0,
+                },
+                new NodeType()
+                {
+                    Name="root",
+                    Id=3,
+                    ParentId=1,
+                }
+
+            };
+            return base.OnInitializedAsync();
+        }
+
+        private void SelectNode()
+        { }
+
+        private void OnDblClick()
+        { }
+    }
+
+    public class NodeType
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+    }
+}
